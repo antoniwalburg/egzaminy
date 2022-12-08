098bac4c0e9ef7b8be2dd6cb4d3cbec9165c2c5d
Antoni Walburg <103273798+antoniwalburg@users.noreply.github.com>
2022-12-08 10:13:14 +0100

    Add files via upload

---
+++ airport.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title> Odloty samolotów </title>
+        <link rel="stylesheet" type="text/css" href="styl6.css" /> 
+    </head>
+    <body>
+        <header>
+            <div id='headerLeft'> Odloty z lotniska
+            </div>
+            <div id='headerRight'>
+            </div>
+        </header>
+        <section>
+            <table>
+            </table>
+        </section>
+        <footer>
+            <div id='footerMid'> Miło nam że nas znowu odwiedziłeś </div>
+            <div id='footerLeft'> wefwefwfw</div>
+            <div id='footerRight'>feffeef </div>
+        </footer>
+    </body>
+</html>
